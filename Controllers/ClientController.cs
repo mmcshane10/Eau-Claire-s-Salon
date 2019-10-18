fc5f5549eb9970abb67f029ea9796ae54e422f95
Mike <mmcshane10@gmail.com>
2019-10-18 10:37:11 -0700

    add create form for clients and redirect to action to clients stylist

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -5,11 +5,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
-    public class ClientController : Controller
+    public class ClientsController : Controller
     {
+        private readonly HairSalonContext _db;
 
+        public ClientsController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.StylistID = new SelectList(_db.Stylists, "StylistID", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            _db.Clients.Add(client);
+            _db.SaveChanges();
+            int id = client.StylistID;
+            return RedirectToAction("Details", "Stylists", new { id });
+        }
     }
 }
