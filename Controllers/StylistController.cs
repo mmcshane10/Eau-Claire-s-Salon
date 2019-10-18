55a90022de46c0a9bf2df017ece5530e4a0a39ba
Mike <mmcshane10@gmail.com>
2019-10-18 09:30:00 -0700

    add ability to add stylists to database and print out onto DOM

--- Controllers/StylistController.cs
+++ Controllers/StylistController.cs
@@ -8,8 +8,34 @@ using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
-    public class StylistController : Controller
+    public class StylistsController : Controller
     {
+        private readonly HairSalonContext _db;
 
+        public StylistsController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Stylist> model = _db.Stylists.ToList();
+            model = model.OrderBy(s => s.Name).ToList();
+
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Stylist stylist)
+        {
+            _db.Stylists.Add(stylist);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
