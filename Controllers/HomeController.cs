a6a45f3c0c23efb78d65061a159537e5910b711b
Mike <mmcshane10@gmail.com>
2019-10-18 08:19:21 -0700

    MVC project layout

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+    }
+}
---
+++ Controllers/StylistController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+    public class StylistController : Controller
+    {
+
+    }
+}
